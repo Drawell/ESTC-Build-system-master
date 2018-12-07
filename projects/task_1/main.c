73e52c6499ad293e6fef5c075b96a53a086d5bce
Ivan <ivanevgnikonov@gmail.com>
2018-12-07 21:03:18 +0300

    [WIP] Task_1 - refactoring

--- projects/task_1/main.c
+++ projects/task_1/main.c
@@ -27,8 +27,8 @@ void init_button(void)
   GPIO_Init(GPIOD, &GPIO_InitStructure); 
 }
 
-uint8_t change_dir_flag = 0;
 uint8_t button_pushed = 0;
+uint8_t direction = 1;
 
 void delay(uint32_t delay_ticks){
     for (uint32_t i = 0; i < delay_ticks; i++){
@@ -37,7 +37,7 @@ void delay(uint32_t delay_ticks){
         }
         else if (button_pushed) {
             button_pushed = 0;
-            change_dir_flag = 1;
+            direction = !direction;
             for (uint16_t j = 0; j < 20000; j++);
             return; 
         }
@@ -50,28 +50,13 @@ int main(void)
     init_button();
 
     const uint16_t arr_of_leds[4] = { GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15 };
-    uint8_t direction = 1;
-
     uint8_t current_led = 0;
 
     while (1)
     {
-        if (change_dir_flag){
-            change_dir_flag = 0;
-            direction = !direction;
-        }
-
-        if (direction){
-            GPIO_ResetBits(GPIOD, arr_of_leds[current_led]);
-            current_led = (current_led + 1) % 4;
-            GPIO_SetBits(GPIOD, arr_of_leds[current_led]);
-            delay(SWITCH_DELAY);
-        }
-        else {
-            GPIO_ResetBits(GPIOD, arr_of_leds[current_led]);
-            current_led = (current_led + 3) % 4;
-            GPIO_SetBits(GPIOD, arr_of_leds[current_led]);
-            delay(SWITCH_DELAY);
-        }
+        GPIO_ResetBits(GPIOD, arr_of_leds[current_led]);
+        current_led = (current_led + 1 + 2 * direction) % 4;
+        GPIO_SetBits(GPIOD, arr_of_leds[current_led]);
+        delay(SWITCH_DELAY);
     }
 }
